4912f502f7a8a3e9b884f9dac691c52e2d2dd5ae
baileymonson <90432762+baileymonson@users.noreply.github.com>
2022-03-21 17:23:27 -0600

    bailey testing

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Temple.Models;
-
+//test
 namespace Temple
 {
     public class Startup
