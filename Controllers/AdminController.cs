cf60d277bdc4fcc7c1ec6b336eb6c9fcd07bd621
emily12jensen <90439014+emily12jensen@users.noreply.github.com>
2022-03-21 22:15:07 -0600

    admin page showing appointments

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Temple.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,14 +12,28 @@ namespace Temple.Controllers
 {
     public class AdminController : Controller
     {
-       
+        private AppointmentContext context { get; set; }
+
+        public AdminController(AppointmentContext temp) => context = temp;
+
         public IActionResult Appointment()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult ViewAppointments()
         {
-            return View();
+            var blah = context.Appointment
+                .Where(x => x.Available == false)
+                .OrderBy(x => x.Date)
+                .ToList();
+           
+
+            return View(blah);
         }
+        //public async Task UpdateData()
+        //{
+        //    AppointmentData =  context.Appointment.ToListAsync();
+        //}
     }
 }
